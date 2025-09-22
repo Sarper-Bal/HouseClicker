d223e600292830307a55809dff4a803524f8f36f
Sarper-Bal <49680723+Sarper-Bal@users.noreply.github.com>
2025-09-22 16:33:47 +0300

    5.2
    logic bazı hatlar dışında tamam, ilk açılına oyun gelmiyo, oyunda sdece 0,0 üretiyo

--- Assets/_Scripts/MiniGames/MiniGameListItem.cs
+++ Assets/_Scripts/MiniGames/MiniGameListItem.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System; // Action için gerekli
 
 public class MiniGameListItem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Image gameIconImage;
+    [SerializeField] private TextMeshProUGUI gameNameText;
+    [SerializeField] private TextMeshProUGUI gameInfoText;
+    [SerializeField] private Button itemButton;
+
+    // Bu elemanı dolduracak olan bilgileri dışarıdan alır ve UI'ı günceller.
+    public void Setup(Sprite icon, string name, string info)
     {
-        
+        gameIconImage.sprite = icon;
+        gameNameText.text = name;
+        gameInfoText.text = info;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Bu elemana tıklandığında ne olacağını dışarıdan belirlememizi sağlar.
+    public void AddClickListener(Action listener)
     {
-        
+        itemButton.onClick.AddListener(() => listener());
     }
 }
