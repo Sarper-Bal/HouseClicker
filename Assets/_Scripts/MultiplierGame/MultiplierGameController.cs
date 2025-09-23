d0a4a0e10b5873e83b2b5767b82218b71029dc6f
Sarper-Bal <49680723+Sarper-Bal@users.noreply.github.com>
2025-09-23 14:07:31 +0300

    7.7
    UI eklentileri tamam

--- Assets/_Scripts/MultiplierGame/MultiplierGameController.cs
+++ Assets/_Scripts/MultiplierGame/MultiplierGameController.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization; // Sayı formatlama için eklendi
 
 [System.Serializable]
 public class MultiplierBox
@@ -19,13 +20,26 @@ public class MultiplierGameController : MonoBehaviour
     [Header("Oyun Verisi")]
     [SerializeField] private MultiplierGameData gameData;
 
-    [Header("UI Elemanları")]
-    [SerializeField] private GameObject gamePanel;
-    [SerializeField] private Button playButton;
+    [Header("Ana Paneller")]
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject confirmationPanel;
+    [SerializeField] private GameObject gameplayPanel;
+    [SerializeField] private GameObject resultPanel;
+
+    [Header("Onay Paneli UI")]
+    [SerializeField] private TextMeshProUGUI gameNameText;
+    [SerializeField] private TextMeshProUGUI betInfoText;
+    [SerializeField] private Button startGameButton;
     [SerializeField] private Button closeButton;
-    [SerializeField] private TextMeshProUGUI infoText;
+
+    [Header("Oyun Paneli UI")]
     [SerializeField] private TextMeshProUGUI currentAmountText;
     [SerializeField] private List<MultiplierBox> boxes;
+
+    [Header("Sonuç Paneli UI")]
+    [SerializeField] private TextMeshProUGUI betResultText;
+    [SerializeField] private TextMeshProUGUI winningsResultText;
+    [SerializeField] private Button finishAndCloseButton;
 
     private double currentWinnings;
     private bool isGameRunning = false;
@@ -33,18 +47,19 @@ public class MultiplierGameController : MonoBehaviour
 
     private void Start()
     {
-        playButton.onClick.AddListener(StartGame);
-        closeButton.onClick.AddListener(ClosePanel);
+        startGameButton.onClick.AddListener(StartGame);
+        closeButton.onClick.AddListener(CloseMainPanel);
+        finishAndCloseButton.onClick.AddListener(CloseMainPanel);
 
         foreach (var box in boxes)
         {
             box.boxButton.onClick.AddListener(() => OnBoxClicked(box));
         }
 
-        gamePanel.SetActive(false);
+        mainPanel.SetActive(false);
     }
 
-    public void ShowPanel()
+    public void ShowConfirmationPanel()
     {
         if (CurrencyManager.Instance.CurrentGold < gameData.betAmount)
         {
@@ -59,42 +74,33 @@ public class MultiplierGameController : MonoBehaviour
             return;
         }
 
-        gamePanel.SetActive(true);
-        ResetGame();
-    }
+        mainPanel.SetActive(true);
+        confirmationPanel.SetActive(true);
+        gameplayPanel.SetActive(false);
+        resultPanel.SetActive(false);
 
-    private void ResetGame()
-    {
-        infoText.text = $"Bahis: {gameData.betAmount} Altın";
-        currentAmountText.text = "0";
-        boxesOpenedCount = 0;
-
-        foreach (var box in boxes)
-        {
-            box.boxButton.transform.localScale = Vector3.one;
-            box.multiplierText.gameObject.SetActive(false);
-            box.boxButton.interactable = false;
-        }
-
-        playButton.interactable = true;
-        isGameRunning = false;
+        gameNameText.text = gameData.gameName;
+        // --- DEĞİŞİKLİK BURADA ---
+        betInfoText.text = gameData.betAmount.ToString("N0", CultureInfo.InvariantCulture);
     }
 
     private void StartGame()
     {
-        if (isGameRunning) return;
+        confirmationPanel.SetActive(false);
+        gameplayPanel.SetActive(true);
 
+        if (isGameRunning) return;
         isGameRunning = true;
-        playButton.interactable = false;
-        infoText.text = "Kutuları Aç!";
+        boxesOpenedCount = 0;
 
         CurrencyManager.Instance.SpendGold(gameData.betAmount);
         MiniGameManager.Instance.RecordPlayTime(gameData.gameName);
 
         currentWinnings = gameData.betAmount;
-        UpdateAmountText(gameData.betAmount, true); // Başlangıç animasyonu için
+        UpdateAmountText(gameData.betAmount, true);
 
         AssignMultipliersToBoxes();
+        ResetBoxVisuals();
 
         foreach (var box in boxes)
         {
@@ -102,34 +108,11 @@ public class MultiplierGameController : MonoBehaviour
         }
     }
 
-    private void AssignMultipliersToBoxes()
-    {
-        List<float> allMultipliers = new List<float>();
-        allMultipliers.AddRange(gameData.positiveMultipliers);
-        allMultipliers.AddRange(gameData.negativeMultipliers);
-
-        var shuffledMultipliers = allMultipliers.OrderBy(x => Random.value).ToList();
-
-        for (int i = 0; i < boxes.Count; i++)
-        {
-            if (i < shuffledMultipliers.Count)
-            {
-                boxes[i].assignedMultiplier = shuffledMultipliers[i];
-            }
-            else
-            {
-                boxes[i].assignedMultiplier = 1f;
-            }
-        }
-    }
-
     private void OnBoxClicked(MultiplierBox clickedBox)
     {
         if (!isGameRunning) return;
-
         clickedBox.boxButton.interactable = false;
         boxesOpenedCount++;
-
         StartCoroutine(RevealBox(clickedBox));
     }
 
@@ -142,21 +125,16 @@ public class MultiplierGameController : MonoBehaviour
         revealAnim.AppendCallback(() =>
         {
             boxToReveal.multiplierText.gameObject.SetActive(true);
-
-            // --- DEĞİŞİKLİK BURADA ---
             if (multiplier > 0)
             {
                 boxToReveal.multiplierText.text = $"x{multiplier}";
                 boxToReveal.multiplierText.color = Color.green;
             }
-            else // Negatif ise
+            else
             {
-                // Metni "-2x" formatında göster
                 boxToReveal.multiplierText.text = $"{multiplier}x";
                 boxToReveal.multiplierText.color = Color.red;
             }
-            // --- DEĞİŞİKLİK SONU ---
-
             boxToReveal.multiplierText.transform.localScale = Vector3.zero;
         });
         revealAnim.Append(boxToReveal.multiplierText.transform.DOScale(1f, 0.4f).SetEase(Ease.OutBack));
@@ -166,22 +144,13 @@ public class MultiplierGameController : MonoBehaviour
         double previousWinnings = currentWinnings;
         bool isPositive = multiplier > 0;
 
-        if (isPositive)
-        {
-            currentWinnings *= multiplier;
-        }
-        else
-        {
-            // Negatif çarpanın değeri -2 ise 2'ye böl, -3 ise 3'e böl.
-            currentWinnings /= -multiplier;
-        }
+        if (isPositive) currentWinnings *= multiplier;
+        else currentWinnings /= -multiplier;
 
         UpdateAmountText(currentWinnings, isPositive);
 
-
         if (boxesOpenedCount >= boxes.Count)
         {
-            // Son kutudan sonra biraz bekleyip oyunu bitir.
             yield return new WaitForSeconds(1.0f);
             FinishGame();
         }
@@ -191,27 +160,66 @@ public class MultiplierGameController : MonoBehaviour
     {
         isGameRunning = false;
         long finalWinnings = (long)System.Math.Round(currentWinnings);
-        infoText.text = $"Kazanç: {finalWinnings} Altın!";
         CurrencyManager.Instance.AddGold(finalWinnings);
 
-        StartCoroutine(ResetAfterDelay(3f));
+        ShowResultPanel(finalWinnings);
     }
 
-    private IEnumerator ResetAfterDelay(float delay)
+    private void ShowResultPanel(long finalWinnings)
     {
-        yield return new WaitForSeconds(delay);
-        ResetGame();
+        gameplayPanel.SetActive(false);
+        resultPanel.SetActive(true);
+
+        // --- DEĞİŞİKLİK BURADA ---
+        betResultText.text = gameData.betAmount.ToString("N0", CultureInfo.InvariantCulture);
+        winningsResultText.text = finalWinnings.ToString("N0", CultureInfo.InvariantCulture);
+
+        if (finalWinnings >= gameData.betAmount)
+        {
+            winningsResultText.color = Color.green;
+        }
+        else
+        {
+            winningsResultText.color = Color.red;
+        }
     }
 
-    // Metin güncelleme fonksiyonunu animasyonlu hale getirelim.
+    private void CloseMainPanel()
+    {
+        if (isGameRunning) return;
+        mainPanel.SetActive(false);
+    }
+
+    private void AssignMultipliersToBoxes()
+    {
+        List<float> allMultipliers = new List<float>();
+        allMultipliers.AddRange(gameData.positiveMultipliers);
+        allMultipliers.AddRange(gameData.negativeMultipliers);
+        var shuffledMultipliers = allMultipliers.OrderBy(x => Random.value).ToList();
+        for (int i = 0; i < boxes.Count; i++)
+        {
+            boxes[i].assignedMultiplier = (i < shuffledMultipliers.Count) ? shuffledMultipliers[i] : 1f;
+        }
+    }
+
+    private void ResetBoxVisuals()
+    {
+        foreach (var box in boxes)
+        {
+            box.boxButton.transform.localScale = Vector3.one;
+            box.multiplierText.gameObject.SetActive(false);
+            box.boxButton.interactable = false;
+        }
+    }
+
     private void UpdateAmountText(double targetAmount, bool isPositiveChange = true)
     {
-        double startAmount = double.Parse(currentAmountText.text, System.Globalization.NumberStyles.Any);
+        // Başlangıç değerini alırken hata oluşmaması için TryParse kullanalım
+        double.TryParse(currentAmountText.text, NumberStyles.Any, CultureInfo.InvariantCulture, out double startAmount);
 
         DOTween.To(() => startAmount, x => startAmount = x, targetAmount, 0.5f)
-               .OnUpdate(() => currentAmountText.text = ((long)System.Math.Round(startAmount)).ToString("N0"));
+               .OnUpdate(() => currentAmountText.text = ((long)System.Math.Round(startAmount)).ToString("N0", CultureInfo.InvariantCulture));
 
-        // Animasyonlu geri bildirim
         if (isPositiveChange)
         {
             currentAmountText.transform.DOPunchScale(Vector3.one * 0.2f, 0.4f);
@@ -220,11 +228,5 @@ public class MultiplierGameController : MonoBehaviour
         {
             currentAmountText.transform.DOShakePosition(0.4f, new Vector3(10, 0, 0));
         }
-    }
-
-    private void ClosePanel()
-    {
-        if (isGameRunning) return;
-        gamePanel.SetActive(false);
     }
 }
